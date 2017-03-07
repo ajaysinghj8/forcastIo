5d8153d53dcd20ea99c4d174ceea7ed8f036b6b8
Plutazoo <meajaysingh@hotmail.com>
2017-03-07 06:06:39 +0530

    using debug logger

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,10 @@
 import { boot } from './server';
 import {APP_CONFIG} from './Services';
+import * as Debug from 'debug';
+
+const logger = Debug('app:boot');
 
 boot({port: APP_CONFIG.PORT, hostname: APP_CONFIG.HOST}).then(() => {
-  console.log('application booted successfully');
+  logger('application booted successfully');
+  logger(`Listing on  ${APP_CONFIG.HOST || 'localhost'} :  ${APP_CONFIG.PORT}`);
 });
